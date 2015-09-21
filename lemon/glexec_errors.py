15458de5c675964e70bed8706cfa19b499a5ae73
jmsilva <jmsilva@cern.ch>
2015-09-21 16:31:13 +0200

    updating glexec_errors.py to fix glidein to site name mapping

--- lemon/glexec_errors.py
+++ lemon/glexec_errors.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+import htcondor
 import subprocess
 from time import strftime
 from sensorAPI import *
@@ -20,77 +21,13 @@ registerMetric(
    "Find for glexec errors at condor shadow logs",
    "glexec_errors.check_glexec")
 
-def sanitize(sites):
-  # sites that share the same domain
-  # name with other sites(v.g. in2p3 sites)
-  siteRegx = [
-               ["T2_FR_IPHC", "sbgwn.*.in2p3.fr"],
-               ["T2_FR_GRIF_LLR","(polgrid|llrgrwn).*.in2p3.fr" ],
-               ["T2_FR_GRIF_IRFU","wn.*.datagrid.cea.fr"],
-               ["T3_FR_IPNL","lyowork.*.in2p3.fr"],
-               ["T2_FR_CCIN2P3","ccwsge.*.in2p3.fr"],
-               ["T2_CH_CERN",".*.cern.ch"],
-               ["T1_UK_RAL","lcg.*.gridpp.rl.ac.uk"]
-             ]
-
-
-  # remove all sites without a SE defined
-  sites = filter(lambda a: a[1] != '', sites)
-
-  # remove all _DISK, _BUFFER, _MSS instances
-  sites = filter(lambda a: "DISK" not in a[0].upper(), sites)
-  sites = filter(lambda a: "BUFFER" not in a[0].upper(), sites)
-  sites = filter(lambda a: "MSS" not in a[0].upper(), sites)
-
-  idx = 0
-  for site in sites:
-    for reg in siteRegx:
-      if reg[0] == site[0]:
-        sites[idx] = reg
-    idx += 1
-
-  # sites defined at phedex api but removed because they only have _DISK, _BUFFER and _MSS 
-  # instances
-  sites.append(["T1_DE_KIT","c.*.gridka.de"])
-  return sites
-
-def condorStatus(glideInName):
-    condorSt = "UNKNOWN_SITE"
-    try:
-        condorStatusCmd = "condor_status -const 'GLIDEIN_MASTER_NAME == \"%s\"' -af glidein_cmssite"%glideInName
-        condorSt = str(subprocess.Popen(condorStatusCmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read())
-        condorSt = condorSt.strip()
-    except:
-        pass
-
-    if len(condorSt) == 0:
-       condorSt = "UNKNOWN_SITE"
-
-    return condorSt
-
-def domainNameToSiteName(domainToFind,glideInName):
-    phedexDataUrl = 'http://cmsweb.cern.ch/phedex/datasvc/json/prod/nodes'
-    phedexAPI=urlopen(phedexDataUrl)
-    jsonRAW=phedexAPI.readlines()
-    jsonStr=''.join(jsonRAW)
-    jsonList=json.loads(jsonStr)
-    nodes = jsonList['phedex']['node']
-
-    sites = [ [str(site['name']).encode('string-escape'),str(site['se']).encode('string-escape')] for site in nodes ]
-    sites = [ [x[0],x[1].split('.') ] for x in sites ]
-    sitesPerDomain = [ [x[0],".".join(x[1][1:len(x[1])])] for x in sites ]
-    sitesPerDomain = sanitize(sitesPerDomain)
-
-    for oneSite in sitesPerDomain:
-       reg = re.compile(oneSite[1])
-       match = re.search(reg, domainToFind)
-       if match:
-           return oneSite[0]
-
-    # if site was not found in phedex api try to fetch from collector using the glideinName
-    # condor_status -const 'GLIDEIN_MASTER_NAME == "glidein_3215_231573213@lnxfarm263.colorado.edu"' -af glidein_cmssite
-    return condorStatus(glideInName)
-
+def glideInNameToSiteName(glideInName):
+   coll = htcondor.Collector('vocms099.cern.ch')
+   constraint = 'Name == "'+glideInName+'"'
+   results = coll.query(htcondor.AdTypes.Startd, constraint, ["glidein_cmssite"])
+   if results:
+      return results[0]['glidein_cmssite']
+   return 'UNKNOWN_SITE'
 
 class check_glexec(Metric):
     '''
@@ -125,6 +62,7 @@ class check_glexec(Metric):
             startH = '0'+startH
 
         timeStamp = timeStamp + startH + ":" + startM
+
         self.log(SENSOR_LOG_INFO, "Time used: %s"%(timeStamp))
 
         glexec_reg = '%s.*ERROR "Error from (.*): error changing sandbox ownership to the user: condor_glexec_setup exited with'%(timeStamp)
@@ -160,9 +98,7 @@ class check_glexec(Metric):
                 errorCount = errorCount[0:19]
 
             for host in errorCount:
-                domainToFind = host[1].split('.')
-                domainToFind = '.'.join(domainToFind[1:len(domainToFind)])
-                domainName = domainNameToSiteName(domainToFind,host[1])
+                domainName = glideInNameToSiteName(host[1])
                 self.storeSample01("%d %s %s"%(host[0],host[1],domainName))
 
         else:
